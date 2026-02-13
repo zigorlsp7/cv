0923759aa310d5ce3e6582416cebd2b2be961a9d
zlz104107 <zigor.lopez@dehn.de>
2026-02-13 19:33:54 +0100

    ci: improve ci workflows

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,12 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: "standalone",
+  turbopack: {
+    root: path.join(__dirname, "../.."),
+  },
 };
 
 export default nextConfig;
